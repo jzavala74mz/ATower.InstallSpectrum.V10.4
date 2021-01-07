9fbf2b65f76973189ec4c9a7fa3312be1c80a005
root <root@atower.labrhel8>
2021-01-07 13:25:07 -0800

    Codificación de los programas

--- InstallLineaComando.sh
+++ InstallLineaComando.sh
@@ -9,7 +9,7 @@ git commit -m "Codificación de los programas"
 
 
 echo "Ejecutar la instalación de Spectrum 10.4 mediante el PlayBook"
-ansible-playbook InstallSpectrum.v10.4.yml -vvv --extra-vars "hostname_name=localhost.localdomain hostname__pretty=localhost redhat_user=jzavala74mz redhat_pass=V4l3r1a20 host_user=jzavala74mz host_pass=P4ssw0rd host_name=192.168.112.133  and flag_suscribir=NO"
+ansible-playbook InstallSpectrum.v10.4.yml -vvv --extra-vars "hostname_name=localhost.localdomain hostname__pretty=localhost redhat_user=jzavala74mz redhat_pass=V4l3r1a20 host_user=jzavala74mz host_pass=P4ssw0rd host_name=192.168.112.133 flag_suscribir=NO user_owner=jzavala74mz"
 echo "Instalación de spectrum 10.4 fionalizada. Verifique los resultados"
 
 git push
