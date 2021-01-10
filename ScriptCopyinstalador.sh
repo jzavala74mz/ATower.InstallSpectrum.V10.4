9e0f476f9b999086d8328e5aeef99a4a2a2c4147
root <root@atower.vmwarelab>
2021-01-10 17:31:22 -0600

    Implementación de la espera de prompts

--- ScriptCopyinstalador.sh
+++ ScriptCopyinstalador.sh
@@ -1,6 +1,18 @@
 # spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
 spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/SpectrumInstall_10.4_Linux.tar.gz /root/SpectrumInstall_10.4_Linux.tar.gz
-expect "password:"
-send P4ssw0rd\n;
-interact
+
+expect {
+      "Are you sure you want to continue connecting (yes/no)?" {
+            send "yes\r"
+            exp_continue
+      }
+      "password:" {
+            send "P4ssw0rd\r"
+            interact
+      }
+}
+
+## expect "password:"
+## send P4ssw0rd\n;
+## interact
 
