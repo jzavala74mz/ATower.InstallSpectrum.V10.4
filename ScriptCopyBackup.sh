b754314208bdc98dd3572ab2f79d5cafa0933a78
root <root@atower.vmwarelab>
2021-01-14 09:40:37 -0600

    Implementación de la funcionalidad de recuperación de base de datos y bandera para activar esta funcionalidad

---
+++ ScriptCopyBackup.sh
@@ -0,0 +1,15 @@
+# spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
+spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/db_20210110_1307.SSdb /root/db_20210110_1307.SSdb
+
+expect {
+      "Are you sure you want to continue connecting (yes/no)?" {
+            send "yes\r"
+            exp_continue
+      }
+      "password:" {
+            send "P4ssw0rd\r"
+            interact
+      }
+}
+
+
