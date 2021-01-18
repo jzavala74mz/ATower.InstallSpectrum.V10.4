baa08a860527ea3e031a1aadeb29517577331b08
root <root@atower.vmwarelab>
2021-01-18 09:54:08 -0600

    Implementación de la restauración de la topologia anterior

--- ScriptCopyBackup.sh
+++ ScriptCopyBackup.sh
@@ -1,5 +1,7 @@
-# spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
-spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/db_20210110_1307.SSdb /root/db_20210110_1307.SSdb
+## spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
+## spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/db_20210110_1307.SSdb /root/db_20210110_1307.SSdb
+spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/salida.xml /root/salida.xml
+
 
 expect {
       "Are you sure you want to continue connecting (yes/no)?" {
